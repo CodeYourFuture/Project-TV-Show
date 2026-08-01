60266dbcf1626755a8b0fb19b8b7b59cc9989a9d
Arthur <>
2026-08-01 16:59:11 +0100

    try pull request

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ function setup() {
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  //test pull request
 }
 
 window.onload = setup;
