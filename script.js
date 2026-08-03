9834a918155a5946b473b3e91f93e458be9063ed
Tobias <amaechinatobias@gmail.com>
2026-08-03 13:23:28 +0100

    create and image element img

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ function makePageForEpisodes(episodeList) {
     const card = document.createElement("article");
      const title = document.createElement("h2");
      const code = document.createElement("p");
+     const image = document.createElement("img");
 
      title.textContent = episode.name;
      code.textContent = episodeCode;
