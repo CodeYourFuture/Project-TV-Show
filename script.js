73141342c268f7c60ae0dcd085ee09b5aa3290b9
Tobias <amaechinatobias@gmail.com>
2026-08-03 14:22:29 +0100

    Used one element to create elemet that  holds name and code,

--- script.js
+++ script.js
@@ -17,27 +17,29 @@ function makePageForEpisodes(episodeList) {
     
     const card = document.createElement("article");
      const title = document.createElement("h2");
-     const code = document.createElement("p");
      const image = document.createElement("img");
      image.src = episode.image.medium;
      image.alt = episode.name;
      const summary = document.createElement("div");
      summary.innerHTML = episode.summary;
 
-     title.textContent = episode.name;
-     code.textContent = episodeCode;
+     title.textContent = `${episode.name}-${episodeCode}`;
+     
      const link = document.createElement("a");
      link.href = episode.url;
      link.textContent = "View on TVMaze";
      link.target = "_blank";
      link.rel = "noopener noreferrer";
 
-    card.appendChild(link);
-     card.appendChild(summary);
-     card.appendChild(image);
-     card.appendChild(code);
+    
+     
+    
+     
      card.appendChild(title);
-     rootElem.appendChild(card);
+      card.appendChild(image);
+      card.appendChild(summary);
+      card.appendChild(link);
+      rootElem.appendChild(card);
    });
   }
 
