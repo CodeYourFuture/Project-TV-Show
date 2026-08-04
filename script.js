c11cbb694e9c68a9e11756773816fbba2bfcf2a8
Martin Mwaka <martinmwaka@hotmail.com>
2026-08-04 20:44:29 +0100

    update script.js so that all films render

--- script.js
+++ script.js
@@ -3,70 +3,43 @@
 //This content is from https://www.tvmaze.com/
 //specifically: https://api.tvmaze.com/shows/82/episodes
 
-const filmGrid = document.getElementById('film-grid');
+const searchInput = document.getElementById('film-search');
 const allEpisodes = getAllEpisodes();
+
+function setup() {
+  renderFilms();
+}
 
 // formats episode and season numbers to show 2 digits
 const formatEpisodeCode = (prefix, value) =>
-  `${prefix}${String(value).padStart(2, "0")}`;
+  `${prefix}${String(value).padStart(2, '0')}`;
 
 const createFilmCard = (film) => {
-  const filmCard = document.getElementById("film-card").content.cloneNode(true);
-  // Use class selectors instead of tag selectors for title
-  const title = filmCard.querySelector(".title");
-  title.innerText = `${film.name} - ${formatEpisodeCode(
-    "S",
-    film.season
-  )}${formatEpisodeCode("E", film.number)}`;
+  const {
+    name,
+    season,
+    number,
+    image: { medium },
+    summary,
+  } = film;
+  const filmCard = document.getElementById('film-card').content.cloneNode(true);
+  const title = filmCard.querySelector('h2');
+  title.innerText = `${name} - ${formatEpisodeCode('S', season)}${formatEpisodeCode('E', number)}`;
 
-  const filmImage = filmCard.querySelector("img");
-  filmImage.src = film.image.medium;
-  filmImage.alt = "image from film";
-  //Use class selectors instead of tag selectors for summary
-  const filmSummary = filmCard.querySelector(".summary");
-  filmSummary.innerHTML = film.summary;
-const formatEpisodeCode = (prefix, value) => {
-  return `${prefix}${String(value).padStart(2, 0)}`;
-};
-
-const createFilmCard = ({
-  name,
-  number,
-  season,
-  summary,
-  image: { medium },
-}) => {
-  const filmCardTemplate = document
-    .getElementById('film-card')
-    .content.cloneNode(true);
-  const filmTitle = filmCardTemplate.querySelector('h2');
-  filmTitle.innerText = `${name} - ${formatEpisodeCode('S', season)}${formatEpisodeCode('E', number)}`;
-
-  const filmImage = filmCardTemplate.querySelector('img');
+  const filmImage = filmCard.querySelector('img');
   filmImage.src = medium;
   filmImage.alt = 'image from film';
 
-  const filmSummary = filmCardTemplate.querySelector('.summary');
+  const filmSummary = filmCard.querySelector('p');
   filmSummary.innerHTML = summary;
 
-  return filmCardTemplate;
+  return filmCard;
 };
 
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("film-grid");
-  const filmCards = episodeList.map(createFilmCard);
-  // Clearing the rootElem(the page )before appending the film cards,so if we rerender the cards again we delete first then append.
-  rootElem.innerHTML = "";
-  rootElem.append(...filmCards);
-function setup() {
-  render();
-}
-
-const render = () => {
+const renderFilms = () => {
+  const rootElem = document.getElementById('film-grid');
   const filmCards = allEpisodes.map(createFilmCard);
-  console.log(filmCards);
-  filmGrid.innerHTML = '';
-  filmGrid.append(...filmCards);
+  rootElem.append(...filmCards);
 };
 
 window.onload = setup;
