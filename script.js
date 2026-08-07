01331c758d7a92d89f52df0a7c1424c4210cb0db
russom <russom.g.meskel@gmail.cpm>
2026-08-07 13:21:17 +0100

    Elements: create elements for tv show title and summary

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 //You can edit ALL of the code here
-function setup() {
+
+console.log("to display?");
+
+const tvShow = getOneEpisode();
+
+const root = document.getElementById("root");
+
+//film.forEach((film) => {
+  const filmCard = document.createElement("section");
+  
+  const title = document.createElement("h1");
+  const summary = document.createElement("p");
+
+  summary.textContent = tvShow.summary;
+  title.textContent = tvShow.name;
+  filmCard.appendChild(title);
+  filmCard.appendChild(summary);
+
+  root.appendChild(filmCard);
+//});
+
+/*function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
@@ -9,4 +30,4 @@ function makePageForEpisodes(episodeList) {
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
 }
 
-window.onload = setup;
+window.onload = setup;*/
