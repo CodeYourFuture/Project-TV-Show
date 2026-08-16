fbd0f15c83509a82e7db9e8495abc62c648d649a
vmoratti <vmoratti@gmail.com>
2026-08-16 11:14:06 +0100

    write makePageForEpisodes() function

--- script.js
+++ script.js
@@ -7,6 +7,17 @@ function setup() {
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  const template = document.getElementById("episode-template");
+  const paddedSeason = (season) => season.toString().padStart(2, "0");
+  const paddedEpisode = (episode) => episode.toString().padStart(2, "0");
+  episodeList.forEach((episode) => {
+    const clone = template.content.cloneNode(true);
+    clone.querySelector("h3").textContent = `${episode.name} - S${paddedSeason(episode.season)}E${paddedEpisode(episode.number)}`;
+    clone.querySelector("img").src = episode.image.medium;
+    clone.querySelector("p").textContent = episode.summary;
+    rootElem.appendChild(clone);
+  });
 }
+
 
 window.onload = setup;
